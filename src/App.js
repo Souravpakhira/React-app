cea403d7c441d0561906e4ce039d6acddf7e5fdc
Souravpakhira <pakhira55@gmail.com>
2020-03-31 19:14:13 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Axios from "axios";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  // set the state
+  state = {
+    confirmed: 0,
+    deaths: 0,
+    recovered: 0,
+
+    countries: []
+  };
+  // Loads the data in the component
+  componentDidMount() {
+    this.getData();
+  }
+
+  // fetch the data from the api and set the state
+  async getData() {
+    const res = await Axios.get("https://souravcovid19api.herokuapp.com/total");
+    const countries = await Axios.get(
+      "https://covid19.mathdro.id/api/countries"
+    );
+    console.log(res);
+
+    this.setState({
+      confirmed: res.data.Confirmed,
+      recovered: res.data.Cured,
+      deaths: res.data.Death,
+
+      countries: countries
+    });
+  }
+  async getCountriesData() {
+    const resCountries = await Axios.get(
+      "https://covid19.mathdro.id/api/countries"
+    );
+    const countries = [];
+    for (var i = 0; i < resCountries.data.countries.length; i++) {
+      countries.push(resCountries.data.countries[i].name);
+    }
+  }
+
+  //   renderCountryOptions() {
+  //     return this.state.countries.map((country, i) => {
+  //       return <option key={i}>{country} </option>;
+  //     });
+  //   }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Covid-19 Daily Update</h1>
+
+        <div className="box confirmed">
+          <h3>Confirmed cases</h3>
+          <h4>{this.state.confirmed}</h4>
+        </div>
+
+        <div className="box deaths">
+          <h3>Death cases</h3>
+          <h4>{this.state.deaths}</h4>
+        </div>
+
+        <div className="box recovered">
+          <h3>Recovered cases</h3>
+          <h4>{this.state.recovered}</h4>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
